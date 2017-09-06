0ef7f6fcf40c2ab48cb501033247acce925bde1e
vstelmashchuk <v.stelmashchuk@mobidev.biz>
2017-09-06 21:40:42 +0300

    rename controllers.

--- src/main/java/com/smartextension/backend/rest/ExtensionController.kt
+++ src/main/java/com/smartextension/backend/rest/ExtensionController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(value = "/extensions")
-class ExtensionControler {
+class ExtensionController {
 
     @Autowired
     lateinit var extensionRepository: ExtensionRepository
