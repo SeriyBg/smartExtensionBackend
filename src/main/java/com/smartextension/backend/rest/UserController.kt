3fcc9c8bd82bb76f4059de9ed470939e339d320e
vstelmashchuk <v.stelmashchuk@mobidev.biz>
2017-09-11 13:23:13 +0300

    add config for google auth and add port config to the application.yml file.

--- src/main/java/com/smartextension/backend/rest/UserController.kt
+++ src/main/java/com/smartextension/backend/rest/UserController.kt
@@ -1,5 +1,10 @@
 package com.smartextension.backend.rest
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = "/sockets")
 class UserController {
     
 }
